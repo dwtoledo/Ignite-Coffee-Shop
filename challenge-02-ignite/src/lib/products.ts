f8ed6dcefbad63d949d8d57843e6748a4c83e8ec
Douglas Toledo <dwtoledo@outlook.com>
2023-09-26 14:46:00 -0400

    Add imgAltText on product model

--- challenge-02-ignite/src/lib/products.ts
+++ challenge-02-ignite/src/lib/products.ts
@@ -15,13 +15,14 @@ import MochaccinoImgUrl from '../assets/images/type-mochaccino.svg'
 
 type Tag = 'traditional' | 'ice' | 'with milk' | 'special' | 'alcoholic'
 
-interface Product {
+export interface Product {
   id: string
   name: string
   tags: Array<Tag>
   description: string
   price: number
   imgUrl: string
+  imgAltText: string
 }
 
 export const products: Array<Product> = [
@@ -32,6 +33,8 @@ export const products: Array<Product> = [
     description: 'The traditional coffee made with hot water and ground beans',
     price: 7.24,
     imgUrl: ExpressoImgUrl,
+    imgAltText:
+      'Traditional Espresso in a cup with white saucer seen from above',
   },
   {
     id: 'a63e6839-1b53-491b-b950-2cf254442bed',
@@ -40,6 +43,7 @@ export const products: Array<Product> = [
     description: 'Diluted espresso, less intense than the traditional one',
     price: 8.15,
     imgUrl: AmericanImgUrl,
+    imgAltText: 'American Espresso in a cup with white saucer seen from above',
   },
   {
     id: 'd5f88de6-d886-4025-bfe1-339c92267b7c',
@@ -48,6 +52,7 @@ export const products: Array<Product> = [
     description: 'Traditional espresso coffee with creamy foam',
     price: 6.89,
     imgUrl: CreamyExpressoImgUrl,
+    imgAltText: 'Creamy Espresso in a cup with white saucer seen from above',
   },
   {
     id: 'bfe6f04f-0a46-4464-bf2c-499ef8f962bb',
@@ -56,6 +61,7 @@ export const products: Array<Product> = [
     description: 'Beverage prepared with espresso and ice cubes',
     price: 7.93,
     imgUrl: IcedExpressoImgUrl,
+    imgAltText: 'Iced Espresso in a cup with white saucer seen from above',
   },
   {
     id: 'a5b462ca-e666-401e-bba5-df412da045e2',
@@ -64,6 +70,7 @@ export const products: Array<Product> = [
     description: 'Half and half of traditional espresso with steamed milk',
     price: 9.47,
     imgUrl: CoffeeWithMilkImgUrl,
+    imgAltText: 'Coffee with Milk in a cup with white saucer seen from above',
   },
   {
     id: '2c7a86e8-499d-4087-b0e4-6af160daa257',
@@ -73,6 +80,7 @@ export const products: Array<Product> = [
       'A shot of espresso with double the amount of milk and creamy foam',
     price: 6.72,
     imgUrl: LatteImgUrl,
+    imgAltText: 'Latte in a cup with white saucer seen from above',
   },
   {
     id: '9a616c3e-d6b6-4883-8c3f-03f2a4f47dd5',
@@ -82,6 +90,7 @@ export const products: Array<Product> = [
       'A cinnamon-infused drink made from equal parts of coffee, milk, and foam',
     price: 8.61,
     imgUrl: CapuccinoImgUrl,
+    imgAltText: 'Cappuccino in a cup with white saucer seen from above',
   },
   {
     id: 'a5106340-bceb-4440-83d5-2e1c451a403d',
@@ -90,6 +99,7 @@ export const products: Array<Product> = [
     description: 'Espresso coffee mixed with a bit of hot milk and foam',
     price: 9.12,
     imgUrl: MacchiatoImgUrl,
+    imgAltText: 'Macchiato in a cup with white saucer seen from above',
   },
   {
     id: 'c9038346-8c70-43b4-a53a-74a2ac9597b2',
@@ -98,6 +108,7 @@ export const products: Array<Product> = [
     description: 'Espresso coffee with chocolate syrup, little milk, and foam',
     price: 7.76,
     imgUrl: MochaccinoImgUrl,
+    imgAltText: 'Mocaccino in a cup with white saucer seen from above',
   },
   {
     id: '5bf6908b-0821-45f8-8ee0-7a6df2f53a4a',
@@ -106,6 +117,7 @@ export const products: Array<Product> = [
     description: 'Drink made with chocolate dissolved in hot milk and coffee',
     price: 8.34,
     imgUrl: HotChocolateImgUrl,
+    imgAltText: 'Hot Chocolate in a cup with white saucer seen from above',
   },
   {
     id: '8bb15987-9045-4fda-b336-b03b0b0ee540',
@@ -114,6 +126,7 @@ export const products: Array<Product> = [
     description: 'Iced espresso drink with rum, cream, and mint',
     price: 9.89,
     imgUrl: CubanImgUrl,
+    imgAltText: 'Cuban coffee in a cup with white saucer seen from above',
   },
   {
     id: '4875e694-f2ff-414e-9ec9-4b9ec7750fec',
@@ -122,6 +135,7 @@ export const products: Array<Product> = [
     description: 'Sweetened drink prepared with coffee and coconut milk',
     price: 6.57,
     imgUrl: HawaiianImgUrl,
+    imgAltText: 'Hawaiian coffee in a cup with white saucer seen from above',
   },
   {
     id: 'faa5da48-1c7f-4f4d-b6fd-9ead92ad8825',
@@ -130,6 +144,7 @@ export const products: Array<Product> = [
     description: 'Beverage prepared with Arab coffee beans and spices',
     price: 7.88,
     imgUrl: ArabicImgUrl,
+    imgAltText: 'Arabic coffee in a cup with white saucer seen from above',
   },
   {
     id: '34fe500e-97d4-49d9-8ff7-0c2f94868964',
@@ -139,5 +154,6 @@ export const products: Array<Product> = [
       'Coffee-based drink with Irish whiskey, sugar, and whipped cream',
     price: 8.73,
     imgUrl: IrishImgUrl,
+    imgAltText: ' in a cup with white saucer seen from above',
   },
 ]
