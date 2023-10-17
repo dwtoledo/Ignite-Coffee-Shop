805b3fa39eee1cf5971e98442b53bb498a49f632
Douglas Toledo <dwtoledo@outlook.com>
2023-10-17 18:29:20 -0400

    Add getReadablePayment function on payment lib file

--- src/lib/payments.ts
+++ src/lib/payments.ts
@@ -3,3 +3,16 @@ export enum PaymentTypes {
   DEBIT_CARD = '1',
   CASH = '2',
 }
+
+export function getReadablePayment(type: string | null): string {
+  switch (type) {
+    case PaymentTypes.CREDIT_CARD:
+      return 'Credit Card'
+    case PaymentTypes.DEBIT_CARD:
+      return 'Debit Card'
+    case PaymentTypes.CASH:
+      return 'Cash'
+    default:
+      return 'Payment type informed on checkout.'
+  }
+}
